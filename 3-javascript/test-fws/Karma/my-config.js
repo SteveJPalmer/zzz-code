a1e6a981efe37f9599931c4927ebe6d76b99f138
SteveJPalmer <stevej_palmer@hotmail.com>
2017-07-03 09:36:25 +0100

    Karma base code

---
+++ 3-javascript/test-fws/Karma/my-config.js
@@ -0,0 +1,73 @@
+// Karma configuration
+// Generated on Mon Aug 29 2016 17:23:58 GMT+0100 (BST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of code & test files/patterns to load in browser(s)
+    files: [
+      '/Users/steve/work_area/Karma/test/*.js',
+      '/Users/steve/work_area/Karma/src/myCode.js'
+      // '/Users/steve/work_area/Karma/test/PlayerSpec.js',
+      // '/Users/steve/work_area/Karma/test/SpecHelper.js',
+      // '/Users/steve/work_area/Karma/src/*.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9877,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
