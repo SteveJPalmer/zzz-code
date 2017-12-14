44d37f23826ef4b4ab43a8348e2f7e1cd6c8cd28
SteveJPalmer <stevej_palmer@hotmail.com>
2017-12-14 14:56:51 +0000

    code completion via external libraries

---
+++ 6-tools/WebStorm/code-completion-extLibs-node-modules/test.js
@@ -0,0 +1,10 @@
+function test() {
+
+  var x = 1;
+  //test
+  var s = 'string';
+
+  //settings >Lang >JavaScript  - techniques to add code completion (jQuery lib examples)
+  $('.bob').addClass('test');
+
+}
