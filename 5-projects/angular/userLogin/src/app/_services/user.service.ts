eecc776508c58860f29d37b50ef0074160f58cd0
SteveJPalmer <stevej_palmer@hotmail.com>
2018-09-25 10:55:34 +0100

    Angular 6 Authentication

---
+++ 5-projects/angular/userLogin/src/app/_services/user.service.ts
@@ -0,0 +1,18 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '../_models';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+    constructor(private http: HttpClient) { }
+
+  //switch out to global config
+  config = {
+    apiUrl: '/orderWebApi'
+  };
+
+    getAll() {
+        return this.http.get<User[]>(`${this.config.apiUrl}/users`);
+    }
+}
